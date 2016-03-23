c11102e5abb42b646f79acb58a523051b04d3065
anatoly yakovenko <aeyakovenko@gmail.com>
2016-03-22 21:44:13 -0700

    prims

--- graph.c
+++ graph.c
@@ -5,9 +5,14 @@
 #include <limits.h>
 #include "heap.h"
 
+struct node;
+struct edge {
+   struct node *n;
+   unsigned weight;
+};
 struct node {
    int id;
-   struct node **edges;
+   struct edge *edges;
    int nedges;
    int nsize;
 };
@@ -18,18 +23,29 @@ struct dist {
    const struct node *prev;
 };
 
-void connect(struct node *a, struct node *b) {
+void connect(struct node *a, unsigned weight, struct node *b) {
    if(a->nedges == a->nsize) {
       int size = (a->nsize + 1)*2;
-      a->edges = realloc(a->edges, sizeof(struct node*)*size);
+      a->edges = realloc(a->edges, sizeof(a->edges[0])*size);
       assert(a->edges);
-      memset(&a->edges[a->nsize], 0, (size - a->nsize) * sizeof(struct node*));
+      memset(&a->edges[a->nsize], 0, (size - a->nsize) * sizeof(a->edges[0]));
       a->nsize = size;
    }
-   a->edges[a->nedges++] = b;
+   a->edges[a->nedges].weight = weight;
+   a->edges[a->nedges].n = b;
+   a->nedges++;
 }
 
-void bruteforce(struct dist *ds, const struct node *s, int depth) {
+void node_dtor(struct node *a) {
+   if(a->nsize) {
+      free(a->edges);
+   }
+   a->nsize = 0;
+   a->nedges = 0;
+   a->id = -1;
+}
+
+void bruteforce_distance(struct dist *ds, const struct node *s, int depth) {
    int i;
    struct dist *sd = &ds[s->id];
    if(depth == 0) {
@@ -37,16 +53,16 @@ void bruteforce(struct dist *ds, const struct node *s, int depth) {
    }
    sd->visited = 1;
    for(i = 0; i < s->nedges; ++i) {
-      struct node *n = s->edges[i];
-      struct dist *nd = &ds[n->id];
-      if(nd->distance > sd->distance + 1) {
+      struct edge *e = &s->edges[i];
+      struct dist *nd = &ds[e->n->id];
+      if(nd->distance > sd->distance + e->weight) {
          nd->prev = s;
-         nd->distance = sd->distance + 1;
+         nd->distance = sd->distance + e->weight;
       }
    }
    depth--;
    for(i = 0; i < s->nedges; ++i) {
-      bruteforce(ds, s->edges[i], depth);
+      bruteforce_distance(ds, s->edges[i].n, depth);
    }
 }
 int compare(void *ctx, void *a, void *b) {
@@ -55,7 +71,7 @@ int compare(void *ctx, void *a, void *b) {
    int ixb = (int)(uintptr_t)b;
    return sd[ixa].distance - sd[ixb].distance;
 }
-void dijkstras(struct heap *h, struct dist *ds, struct node *nodes) {
+void dijkstras_distance(struct heap *h, struct dist *ds, struct node *nodes) {
    int i;
    const struct node *s;
    struct dist *sd;
@@ -67,60 +83,104 @@ void dijkstras(struct heap *h, struct dist *ds, struct node *nodes) {
    sd = &ds[s->id];
    sd->visited = 1;
    for(i = 0; i < s->nedges; ++i) {
-      struct node *n = s->edges[i];
+      struct edge *e = &s->edges[i];
+      struct node *n = e->n;
       struct dist *nd = &ds[n->id];
-      if(!nd->visited && nd->distance > sd->distance + 1) {
+      if(!nd->visited && nd->distance > sd->distance + e->weight) {
          nd->prev = s;
-         nd->distance = sd->distance + 1;
+         nd->distance = sd->distance + e->weight;
          heap_insert(h, (void*)(uintptr_t)n->id);
       }
    }
-   dijkstras(h, ds, nodes);
+   dijkstras_distance(h, ds, nodes);
 }
 
-int main(void) {
-   int i,a,b;
+void test_dijkstras(void) {
+   int i,a,b,w;
    struct node nodes[10];
    struct dist brute[10];
-   struct dist dikjs[10];
+   struct dist dijks[10];
    struct heap h;
    void *data[10];
    h.data = data;
    h.size = 10;
    h.elems = 0;
-   h.ctx = dikjs;
+   h.ctx = dijks;
    h.pfn = compare;
-
    memset(nodes, 0, sizeof(nodes)); 
    memset(brute, 0, sizeof(brute)); 
-   memset(dikjs, 0, sizeof(dikjs)); 
+   memset(dijks, 0, sizeof(dijks)); 
 
    for(i = 0; i < 10; ++i) {
       nodes[i].id = i;
    }
    for(i = 0; i < 10; ++i) {
       brute[i].distance = INT_MAX;
-      dikjs[i].distance = INT_MAX;
+      dijks[i].distance = INT_MAX;
    }
 
    for(i = 0; i < 50; ++i) {
       a = rand() % 10;
       b = rand() % 10;
-      connect(&nodes[a], &nodes[b]);
+      w = rand() % 10 + 1;
+      connect(&nodes[a], w, &nodes[b]);
    }
    brute[0].distance = 0;
    brute[0].prev = &nodes[0];
-   bruteforce(brute, &nodes[0], 10);
+   bruteforce_distance(brute, &nodes[0], 10);
 
-   dikjs[0].distance = 0;
-   dikjs[0].prev = &nodes[0];
+   dijks[0].distance = 0;
+   dijks[0].prev = &nodes[0];
    heap_insert(&h, (void*)(uintptr_t)0);
-   dijkstras(&h, dikjs, nodes);
-   assert(0 == memcmp(brute, dikjs, sizeof(brute)));
+   dijkstras_distance(&h, dijks, nodes);
    for(i = 0; i < 10; ++i) {
-      printf("brute[%d] prev:%d dist:%d\n", i, brute[i].prev->id, brute[i].distance);
+      assert(brute[i].distance == dijks[i].distance);
+      assert(brute[i].visited == dijks[i].visited);
    }
    for(i = 0; i < 10; ++i) {
-      printf("dikjs[%d] prev:%d dist:%d\n", i, dikjs[i].prev->id, dikjs[i].distance);
+      node_dtor(&nodes[i]);
+   }
+}
+
+void prims_msp_v2(struct node *nodes, struct node *msp, int sz) {
+   int i;
+   struct node *minn = 0;
+   struct edge *mine = 0;
+   int mind = INT_MAX; 
+   for(i = 0; i < sz; ++i) {
+      int k;
+      struct node *n = &nodes[i];
+      if(msp[i].id != i) {
+         //not in the msp yet
+         continue;
+      }
+      for(k = 0; k < n->nedges; ++k) {
+         struct edge *e = &n->edges[k];
+         if(msp[e->n->id].id == e->n->id) {
+            //already an edge to the msp
+            continue;
+         }
+         if(!mine || e->weight < mine->weight) {
+            mine = e;
+            minn = n;
+         }
+      }
+   }
+   if(minn) {
+      assert(mine);
+      //min is in msp
+      assert(msp[minn->id].id == minn->id);
+      //edge is to a node not in msp
+      assert(msp[mine->n->id].id != mine->n->id);
+
+      msp[mine->n->id].id = mine->n->id;
+      connect(&msp[minn->id], mine->weight, &msp[mine->n->id]);
+      prims_msp_v2(nodes, msp, sz);
+   }
+}
+int main(void) {
+   int i;
+   for(i = 0; i < 5; ++i) { 
+      test_dijkstras();
    }
 }
--- heap_u.c
+++ heap_u.c
@@ -17,19 +17,19 @@ int main(void) {
    h.pfn = ints;
    h.ctx = 0;
    for(j = 0; j < 10; ++j) {
-      printf("starting...\n");
+      //printf("starting...\n");
       memset(data,0,sizeof(data));
       assert(h.elems == 0);
       for(i = 0; i < 10; ++i) {
          int v = rand();
-         printf("\tadding %d\n", v);
+         //printf("\tadding %d\n", v);
          heap_insert(&h, (void*)(uintptr_t)v);
       }
       assert(h.elems == 10);
       prev = (int)(uintptr_t)h.data[0];
       for(i = 0; i < 10; ++i) {
          int rv = (int)(uintptr_t)heap_remove(&h); 
-         printf("%d\n", rv);
+         //printf("%d\n", rv);
          assert(prev <= rv);
          prev = rv;
       }
